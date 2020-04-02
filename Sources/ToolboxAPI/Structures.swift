2b088eec3cfc7c3d3c84b7d6b97c7caaaf901ea0
George <georgej1088@gmail.com>
2020-04-02 15:39:01 -0400

    Flesh out usernote structures

--- Sources/ToolboxAPI/Structures.swift
+++ Sources/ToolboxAPI/Structures.swift
@@ -1,6 +1,7 @@
+import Foundation
+
 /// A raw usernote representation.
 struct RawUsernote {
-
     /// Timestamp (seconds since epoch)
     let t: Int?
     /// Note text
@@ -13,3 +14,28 @@ struct RawUsernote {
     let l: String?
 }
 
+/// A usernote type, e.g. "Permanant ban", "Good contributor", etc.
+struct UsernoteType {
+    /// The name of the note type
+    let name: String
+    /// The color associated with this note type
+    let color: String // TODO: return this as a friendlier type
+    /// The internal storage key used for the note type
+    let key: String
+}
+
+/// A single usernote.
+struct Usernote {
+    /// The name of the user associated with the note
+    let user: String
+    /// The text of the note
+    let text: String
+    /// The name of the moderator who left the note
+    let mod: String?
+    /// The type of the note
+    let type: UsernoteType?
+    /// The context link for the note
+    let link: String?
+    /// The date and time of the note's creation
+    let date: Date
+}
