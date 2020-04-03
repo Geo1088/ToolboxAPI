f9d49cef574eef925947747552a09430e49f0c69
George <georgej1088@gmail.com>
2020-04-02 20:00:03 -0400

    Update test data, eliminate warning message

--- Tests/ToolboxAPITests/ToolboxAPITests.swift
+++ Tests/ToolboxAPITests/ToolboxAPITests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import ToolboxAPI
 
 let jsonStrings = ["""
-{
-    "ver": 6,
-    "blob": "eJyrVkpPzTc0sLBQsqpWyitWsoquVipRsjI0NbU0NjQwNzXQUcpTslLyLFEvVkhUKE5NLkotUSjJV0gtSy2qzM9LVdJRKgcq11HKBZK1sbW1AKKCF4Q="
-}
+{"ver":6,"constants":{"users":["geo1088","somebodyElse"],"warnings":[null,"spamwarn"]},"blob":"eJxdzEEKwjAQBdCrhL9xk0WGGk1zA88gUoqOgjSNJEEtJXd3aOnGzXyGz38zHhzJOAc/Y8zwZwl4nMouq15lviYuqkTFb05THBkaBZ6sbRsyR2s0ArzcQUbSfeSpl6o3tuv+4PzqQ5jUEpw2zVl3aN2+WTRatUHfiZ7f26qSqPUH9moxBA=="}
 """]
 
 final class ToolboxAPITests: XCTestCase {
@@ -22,13 +19,15 @@ final class ToolboxAPITests: XCTestCase {
             XCTAssertNotNil(data, "wat")
 
             let otherUsernotes = try! UsernotesData(data: data!)
+            XCTAssertNotNil(otherUsernotes, "wat")
             // TODO: test data equality between usernotes and otherUsernotes
 
-            let username = "geo1088"
+            let username = "geo1088__"
             let notesForUser = usernotes.notes(onUser: username)
             debugPrint(notesForUser)
             XCTAssertEqual(notesForUser[0].user, username, "Username should match")
-            XCTAssertEqual(notesForUser[0].text, "It's a secret to everyone", "Note text should match")
+            XCTAssertEqual(notesForUser[0].text, "spammy spammer", "Note text should match")
+            XCTAssertEqual(notesForUser[0].mod, "somebodyElse", "Mod should match")
         }
     }
 
